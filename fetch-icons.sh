adff216c8dcc6231508ea9ad5baad3b95802a7cb
Benjie Gillam <benjie@jemjie.com>
2012-06-27 12:45:45 +0100

    Icon-fetch: mkdir images; silent.

--- fetch-icons.sh
+++ fetch-icons.sh
@@ -1,3 +1,6 @@
-curl -o pim-osx.icns http://pim-files.s3.amazonaws.com/pim-osx.icns
-curl -o images/pim.svg http://pim-files.s3.amazonaws.com/pim.svg
+if [ ! -d images ]; then
+  mkdir images
+fi
+curl -so pim-osx.icns http://pim-files.s3.amazonaws.com/pim-osx.icns
+curl -so images/pim.svg http://pim-files.s3.amazonaws.com/pim.svg
 
